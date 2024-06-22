1b9744973adcab3f27179254a4255211073ec7a2
tw30006 <original.heart.2016@gmail.com>
2024-06-22 18:09:22 +0800

    add: install tailwindCSS & Font Awesome

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
-import './assets/main.css'
+import './assets/style.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fas)
+
+createApp(App)
+  .component('font-awesome-icon' ,FontAwesomeIcon)
+  .mount('#app')
