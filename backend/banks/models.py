39ae67d5da1f5f1f86887630555ffe96a8005367
tw30006 <original.heart.2016@gmail.com>
2024-06-23 22:15:00 +0800

    add: Create Bank Model & get-data & save data to db.sqlite3

--- backend/banks/models.py
+++ backend/banks/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+import re
 
-# Create your models here.
+class Bank(models.Model):
+    headOfficeCode = models.CharField(max_length=10, default="")
+    branchOfficeCode = models.CharField(max_length=10, blank=True, null=True, default="")
+    headOffice = models.CharField(max_length=100,default="")
+    branchOffice = models.CharField(max_length=100,default="")
+    name = models.CharField(max_length=100,default="")
+    address = models.CharField(max_length=250,default="")
+    tel = models.CharField(max_length=20,default="")
+    
+    def save(self, *args, **kwargs):
+        match = re.search(r'(.*?)(銀行|信用合作社|證券|公司)(股份有限公司)?(.*)', self.name)
+        if match:
+            self.headOffice = match.group(1) + match.group(2)
+            self.branchOffice = re.sub(r'股份有限公司|有限公司|集團', '', match.group(4)).strip()
+        else:
+            self.headOffice = self.name
+            self.branchOffice = ''
+
+        self.address = self.address if self.address else 'null'
+        self.tel = self.tel if self.tel else ''
+
+        super().save(*args, **kwargs)
--- backend/banks/views.py
+++ backend/banks/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from io import StringIO
+from django.views.decorators.http import require_GET
+from django.http import JsonResponse
+from banks.models import Bank
+import pandas as pd
+import requests
 
-# Create your views here.
+
+@require_GET
+def get_data(request):
+    url = "https://stat.fsc.gov.tw/FSC_OAS3_RESTORE/api/CSV_EXPORT?TableID=B14&OUTPUT_FILE=Y"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        csv_data = response.content.decode('utf-8')
+        dataFrame = pd.read_csv(StringIO(csv_data))
+        dataFrame = dataFrame.where(pd.notnull(dataFrame),None)
+        data = dataFrame.to_dict(orient='records')
+
+        for row in data:
+            name = row['機構名稱']
+            address = row['地址'] if row['地址'] else 'null'
+            tel = row['電話'] if row['電話'] else ''
+            headOfficeCode = row['總機構代號']
+            branchOfficeCode = row['機構代號']
+
+            bank = Bank(
+                name = name, 
+                address = address, 
+                tel = tel, 
+                headOfficeCode = headOfficeCode, branchOfficeCode = branchOfficeCode
+            )
+            bank.save()
+            
+        return JsonResponse(data, safe=False)
+    else:
+        return JsonResponse({"error": "Failed to retrieve data"}, status=response.status_code)
