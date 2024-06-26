abc5a00b9ef6dad61d9745b04cbb0214488b938a
tw30006 <original.heart.2016@gmail.com>
2024-06-26 17:37:58 +0800

    fix: ALLOWED_HOSTS

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["yuhsuan.zeabur.app"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
