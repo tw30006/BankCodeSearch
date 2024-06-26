8cd2365d12ecc291f446c036c8d7b40d5665d641
tw30006 <original.heart.2016@gmail.com>
2024-06-26 14:44:31 +0800

    update: allowed_hosts & DEBUG

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n741ql!!t=z$yut7y4l6&31w32au7a#m4bw#$1diobtey6%bu='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
